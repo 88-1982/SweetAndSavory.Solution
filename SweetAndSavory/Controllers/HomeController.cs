de13a3110b02760c41327cf7ca90c74f7c2f2ce5
DeAunte Hall <75832818+88-1982@users.noreply.github.com>
2021-11-04 13:25:23 -0700

    code refactor

--- SweetAndSavory/Controllers/HomeController.cs
+++ SweetAndSavory/Controllers/HomeController.cs
@@ -1,37 +1,26 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Mvc;
 using SweetAndSavory.Models;
+using System.Linq;
 
 namespace SweetAndSavory.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+public class HomeController : Controller
+{
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+private readonly SweetAndSavoryContext _db;
 
-        public IActionResult Index()
-        {
-            return View();
-        }
+public HomeController(SweetAndSavoryContext db)
+{
+_db = db;
+}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+[HttpGet("/")]
+public ActionResult Index()
+{
+ViewBag.Treats = _db.Treats.ToList();
+ViewBag.Flavors = _db.Flavors.ToList();
+return View();
+}
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+}
 }
